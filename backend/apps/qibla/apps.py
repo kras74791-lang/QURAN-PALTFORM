e9d8204025adfafe8c9ab58bf3af50fa256258c5
kras74791-lang <kras74791@gmail.com>
2026-06-26 23:36:44 +0600

    Qibla app configuration

---
+++ backend/apps/qibla/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class QiblaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.qibla'
+    verbose_name = 'Qibla Compass'
