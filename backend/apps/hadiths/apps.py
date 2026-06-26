e3f5041b9c5854c87383e1510d45c5b5dc212d7e
kras74791-lang <kras74791@gmail.com>
2026-06-26 23:38:10 +0600

    Hadiths app configuration

---
+++ backend/apps/hadiths/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class HadithsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.hadiths'
+    verbose_name = 'Hadiths'
